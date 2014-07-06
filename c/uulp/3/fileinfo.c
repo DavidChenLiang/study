74e3c7d76501e4f94fef688308e56d69fe08ef5a
david chen <chenliang.global@gmail.com>
2014-07-06 07:37:58 -0700

    for ls2

--- c/uulp/3/fileinfo.c
+++ c/uulp/3/fileinfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <strings.h>
 
 void show_stat_info(char *,struct stat *);
 
@@ -19,11 +20,23 @@ main(int ac, char * av[]){
 }
 
 void show_stat_info(char * fname, struct stat * buf){
+    printf("    name:%s\n",fname);
     printf("    mode:%o\n",buf->st_mode);
     printf("    links:%d\n",buf->st_nlink);
     printf("    user:%d\n",buf->st_uid);
     printf("    group:%d\n",buf->st_gid);
     printf("    size:%d\n",buf->st_size);
-    printf("    modtime:%d\n",buf->st_mtime);
-    printf("    name:%s\n",fname);
+    time_t t = buf->st_atime;
+    printf("    atime is %ld\n",t);
+    char * cp = ctime(&t);
+    char str[80];
+    if (cp) {
+        strcpy(str,ctime(&t));
+        printf("    cp is not NULL.%s\n",str);
+   
+    }
+     
+    //time(&t);
+    //printf("    last access time:%s\n",ctime(&t));
+
 }
