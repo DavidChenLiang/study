d204314a3e81c06ca606a1fba9c6527981eb2d66
chen liang <chenliang.global@gmail.com>
2015-02-10 22:08:26 +0800

    seg

---
+++ c/seg.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  seg.c
+ *
+ *    Description:  seg example
+ *
+ *        Version:  1.0
+ *        Created:  2015年02月10日 21时46分08秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int
+main(void)
+{
+    int man = 0;
+    scanf("%d",man);
+    return EXIT_SUCCESS;
+
+}
+
+
