bdaac214a1868d789295b570ab14859ba472f647
David Chen Liang <chenliang.global@gmail.com>
2015-07-04 20:15:47 +0800

    modify Makefile change setecho.c

--- c/uulp/5/setecho.c
+++ c/uulp/5/setecho.c
@@ -7,16 +7,16 @@ int
 main(int ac, char *args[])
 {
     struct termios info;
-    if (ac == 1){
-        oops("tcgetattr",1);
-    }
+    if (ac == 1) exit(0);
+    if (tcgetattr(0, &info)==-1)
+	    oops("tcgetattr",1);
     if (args[1][0] == 'y'){
         info.c_lflag |= ECHO;
     } else {
         info.c_lflag &= ~ECHO; 
     }
 
-    if (tcsetattr(0,TCSANOW,&info == -1)){
-        oops("tcgetattr",1);
+    if (tcsetattr(0,TCSANOW,&info) == -1){
+        oops("tcgetattr",2);
     }
 }
