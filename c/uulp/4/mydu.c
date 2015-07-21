d848ece6181dbfe9fd004eed25a16aae191c9f13
David Chen Liang <chenliang.global@gmail.com>
2015-07-21 23:08:57 +0800

    change mydu.c

--- c/uulp/4/mydu.c
+++ c/uulp/4/mydu.c
@@ -20,6 +20,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -33,6 +35,12 @@ main ( int argc, char *argv[] )
 
         getopt(argc,argv,"h");
         printf("%s\n",argv[optind]);
+        struct stat buf;
+        if (stat(argv[optind],&buf) == -1){
+                perror("Can not stat");
+                exit(-1);
+        }
+        printf("size %lld\n",buf.st_size);
         
         return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
