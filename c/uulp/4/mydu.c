4621255a4b66dbe7003112636591c16ad9acc7ee
David Chen Liang <chenliang.global@gmail.com>
2015-07-26 22:31:55 +0800

    mydu.c

--- c/uulp/4/mydu.c
+++ c/uulp/4/mydu.c
@@ -32,14 +32,39 @@
         int
 main ( int argc, char *argv[] )
 {
+        int opt;
+        while (opt = getopt(argc,argv,"h") != -1){
+            switch(opt){
+            case 'h':
+                ;
+            }
 
-        getopt(argc,argv,"h");
-        struct stat buf;
-        if (stat(argv[optind],&buf) == -1){
-                perror("Can not stat");
-                exit(-1);
         }
-        printf("%lld\t%s\n",(unsigned long long)(buf.st_blocks*512)/1024,argv[optind]);
-        
+        struct stat statbuf;
+        if (stat(argv[optind],&statbuf) == -1){
+            perror("Can not stat");
+            exit(-1);
+        }
+        int count =0;
+        if (S_ISDIR(statbuf.st_mode)){
+                DIR * dir_ptr;
+                struct dirent * direntp;
+                if ((dir_ptr = opendir(argv[optind])) == NULL){
+                        fprintf(stderr,"can not open dir %s\n",argv[optind]);
+                }else{
+                        while ((direntp = readdir(dir_ptr)) != NULL){
+//                                printf("%s\n",direntp->d_name);
+                                if (stat(direntp->d_name,&statbuf) != -1){
+                                if (strcmp(direntp->d_name,".") != 0)
+                                    count += (unsigned long long)(statbuf.st_blocks*512)/1024;
+//                                    fprintf(stdout,"%lld\t%s\n",(unsigned long long)(statbuf.st_blocks*512)/1024,argv[optind]);
+                                }
+                        }
+                        printf("%d\n ",count);
+                        closedir(dir_ptr);
+                }
+            }else{
+        printf("%lld\t%s\n",(unsigned long long)(statbuf.st_blocks*512)/1024,argv[optind]);
+            } 
         return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
