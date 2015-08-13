ff1f5ce37eae68a8bf57fe7c5cf18adf7962c0f1
David Chen Liang <chenliang.global@gmail.com>
2015-08-13 22:35:33 +0800

    add

--- c/algo/frames.c
+++ c/algo/frames.c
@@ -21,6 +21,31 @@
 #include    "frames.h"
 #include    "list.h"
 
+int
+alloc_frame(List *frames){
+    int frame_number,*data;
+    if(list_size(frames) == 0)
+        return -1;
+    else{
+        if (list_rem_next(frames,NULL,(void **)&data) != 0)
+            return -1;
+        else{
+            frame_number = *data;
+            free(data);
+        }
+    }
+    return frame_number;
+}
+int
+free_frame(List *frames, int frame_number){
+    int *data;
+    if ((data = (int *)malloc(sizeof(int))) == NULL)
+        return -1;
+    *data = frame_number;
+    if (list_ins_next(frames, NULL,data) != 0)
+        return -1;
+    return 0;
+}
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  main
@@ -30,13 +55,13 @@
     int
 main ( int argc, char *argv[] )
 {
+    List * list = (List *)malloc(sizeof(List));
+    list_init(list,NULL);
+    int *data = (int *)malloc(sizeof(int));
+    *data = 10;
+    list_ins_next(list,NULL,data);
+    free(list);
     return EXIT_SUCCESS;
-}				/* ----------  end of function main  ---------- */
-int
-alloc_frame(List *frame){
-    int frame_number,*data;
-    if(list_size
-            )
 
-    return frame_number;
-}
+}				/* ----------  end of function main  ---------- */
+
